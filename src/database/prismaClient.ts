037b252486cefa4e8ff0c01f6b570cca37895367
marianocossio01-1997 <marianocossio01@gmail.com>
2026-08-19 16:34:51 -0300

    fix: corregir generator provider y url en schema.prisma

--- src/database/prismaClient.ts
+++ src/database/prismaClient.ts
@@ -1,19 +1,5 @@
-import "dotenv/config";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb"; 
-import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaMariaDb({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "mariano154367892",
-    database: "doguber_1",
-    connectionLimit: 10,
-
-    allowPublicKeyRetrieval: true
-});
-
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export default prisma;
-	
