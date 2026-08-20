4de0670524e7295858f8fedd5f274e41c06ea6d5
marianocossio01-1997 <marianocossio01@gmail.com>
2026-08-20 13:04:55 -0300

    fix: importar PrismaClient desde @prisma/client y agregar postinstall generate

--- src/database/prismaClient.ts
+++ src/database/prismaClient.ts
@@ -1,16 +1,6 @@
 import "dotenv/config";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaMariaDb({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "mariano154367892",
-    database: "doguber_1",
-    connectionLimit: 10,
-    allowPublicKeyRetrieval: true
+const prisma = new PrismaClient();
 
-});
-const prisma = new PrismaClient({ adapter });
-export default prisma; 
+export default prisma;
